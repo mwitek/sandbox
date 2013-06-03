4fa3f1aab32606569c7c4cfe827527280fa6befd
mwitek <matt@mwitekdesign.com>
2013-06-02 22:02:17 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Migrationapp::Application.config.secret_token = 'defcfaaf1ee4461fab6080005cdbb4e219894867c077e57e5d62cf943bdbc17cb657d4b5252f5e303f7a4d0946dea0d0667d85d8efdde61a68eecdeaf8942127'
